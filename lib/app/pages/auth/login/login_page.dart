49a7da54dcbe27cd25aa4da97020b06886b2d91f
Barba2k2 <lorenzo.dz@hotmail.com>
2024-06-22 16:36:55 -0300

    Layout of LoginPage finished

--- lib/app/pages/auth/login/login_page.dart
+++ lib/app/pages/auth/login/login_page.dart
@@ -1,15 +1,153 @@
+import 'dart:developer';
+
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../../../core/extension/size_screen_extension.dart';
+import '../../../core/ui/widgets/car_wash_text_form_field.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var mediaQuery = MediaQuery.of(context);
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login Page'),
+      backgroundColor: const Color(0xFFF2F9F9),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 100),
+            child: Container(
+              constraints: BoxConstraints(
+                maxWidth: 650,
+                minHeight: mediaQuery.size.height - 200,
+              ),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Align(
+                      alignment: Alignment.topCenter,
+                      child: Image.asset(
+                        'assets/logo/logo.png',
+                        width: 400,
+                        height: 240,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 60,
+                    ),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 32),
+                        child: Text(
+                          'Login',
+                          style: GoogleFonts.poppins(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 24,
+                    ),
+                    const CarWashTextFormField(
+                      hintText: 'E-mail',
+                      obscureText: false,
+                    ),
+                    const SizedBox(
+                      height: 24,
+                    ),
+                    const CarWashTextFormField(
+                      hintText: 'Senha',
+                      obscureText: true,
+                      showEyeIcon: true,
+                    ),
+                    const SizedBox(
+                      height: 24,
+                    ),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 32),
+                        child: GestureDetector(
+                          onTap: () {},
+                          child: Text(
+                            'Esqueci minha senha',
+                            style: GoogleFonts.archivo(
+                              fontSize: 16,
+                              color: Colors.black,
+                              decoration: TextDecoration.underline,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 24,
+                    ),
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 32),
+                      width: double.infinity,
+                      height: 60,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: context.primaryColor,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                        child: Text(
+                          'Entrar',
+                          style: GoogleFonts.poppins(
+                            fontSize: 24,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    RichText(
+                      text: TextSpan(
+                        text: 'Não tem uma conta? ',
+                        children: [
+                          TextSpan(
+                            text: 'Cadastre-se',
+                            style: GoogleFonts.archivo(
+                              color: Colors.black,
+                              decoration: TextDecoration.underline,
+                            ),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                log('Apertei no cadastre-se');
+                                Get.toNamed('/auth/register');
+                              },
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
-      body: Container(),
     );
   }
 }
