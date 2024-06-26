fbb986d63a710523b9951ae9e3daa3bb0eb9ca62
Barba2k2 <lorenzo.dz@hotmail.com>
2024-06-26 11:35:57 -0300

    chore: Code refactor to easy maintance
    feat: Add ValueNotifier do control page index

--- lib/app/pages/dashboard/widgets/nav_bar.dart
+++ lib/app/pages/dashboard/widgets/nav_bar.dart
@@ -3,14 +3,18 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class NavBar extends StatelessWidget {
-  const NavBar({super.key});
+  final ValueNotifier<int> selectedIndexNotifier;
+
+  const NavBar({
+    super.key,
+    required this.selectedIndexNotifier,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Drawer(
       width: 360,
-      color: Colors.white,
-      padding: const EdgeInsets.symmetric(horizontal: 16),
+      backgroundColor: Colors.white,
       child: Column(
         children: [
           Padding(
@@ -42,180 +46,178 @@ class NavBar extends StatelessWidget {
             mouseCursor: SystemMouseCursors.basic,
           ),
           //$ New Bookings
-          ListTile(
-            leading: const Icon(
-              Icons.fiber_new_rounded,
-              size: 34,
-              color: Color(0XFF555555),
-            ),
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Novos Agendamentos',
-                  style: GoogleFonts.poppins(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w600,
-                    color: const Color(0XFF555555),
-                  ),
-                ),
-                CircleAvatar(
-                  radius: 10,
-                  backgroundColor: Colors.red,
-                  child: Center(
-                    child: Text(
-                      '2',
-                      style: GoogleFonts.poppins(
-                        fontSize: 12,
-                        fontWeight: FontWeight.w600,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            onTap: () {},
+          _buildListTile(
+            context: context,
+            icon: Icons.fiber_new_rounded,
+            title: 'Novos Agendamentos',
+            enableNotifications: true,
+            index: 0,
+            onTap: () {
+              Navigator.of(context).pushNamed('/dashboard/home');
+            },
           ),
           //$ Aceitos
-          ListTile(
-            leading: const Icon(
-              Icons.check_circle_outline_outlined,
-              size: 34,
-              color: Color(0XFF555555),
-            ),
-            title: Text(
-              'Agen. aceitos',
-              style: GoogleFonts.poppins(
-                fontSize: 18,
-                fontWeight: FontWeight.w600,
-                color: const Color(0XFF555555),
-              ),
-            ),
+          _buildListTile(
+            context: context,
+            icon: Icons.check_circle_outline_outlined,
+            title: 'Agen. aceitos',
+            index: 1,
             onTap: () {},
           ),
           //$ Reagendamentos
-          ListTile(
-            leading: const Icon(
-              Icons.sync_rounded,
-              size: 34,
-              color: Color(0XFF555555),
-            ),
-            title: Text(
-              'Agen. Reagendados',
-              style: GoogleFonts.poppins(
-                fontSize: 18,
-                fontWeight: FontWeight.w600,
-                color: const Color(0XFF555555),
-              ),
-            ),
+          _buildListTile(
+            context: context,
+            icon: Icons.sync_rounded,
+            title: 'Agen. Reagendados',
+            index: 2,
             onTap: () {},
           ),
           //$ Users
-          ListTile(
-            leading: const Icon(
-              Icons.person_outline_outlined,
-              size: 34,
-              color: Color(0XFF555555),
-            ),
-            title: Text(
-              'Usuários',
-              style: GoogleFonts.poppins(
-                fontSize: 18,
-                fontWeight: FontWeight.w600,
-                color: const Color(0XFF555555),
-              ),
-            ),
+          _buildListTile(
+            context: context,
+            icon: Icons.person_outline_outlined,
+            title: 'Usuários',
+            index: 3,
             onTap: () {},
           ),
           //$ Settings
-          ListTile(
-            leading: const Icon(
-              Icons.display_settings_rounded,
-              size: 34,
-              color: Color(0XFF555555),
-            ),
-            title: Text(
-              'Configurações',
-              style: GoogleFonts.poppins(
-                fontSize: 18,
-                fontWeight: FontWeight.w600,
-                color: const Color(0XFF555555),
-              ),
-            ),
+          _buildListTile(
+            context: context,
+            icon: Icons.display_settings_rounded,
+            title: 'Configurações',
+            index: 4,
             onTap: () {},
           ),
           const Spacer(),
           //$ Logout
-          Column(
-            children: [
-              Row(
-                children: [
-                  CircleAvatar(
-                    radius: 30,
-                    backgroundColor: Colors.white.withOpacity(0.75),
-                    backgroundImage: const AssetImage(
-                      'assets/images/homer.png',
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    CircleAvatar(
+                      radius: 30,
+                      backgroundColor: Colors.white.withOpacity(0.75),
+                      backgroundImage: const AssetImage(
+                        'assets/images/homer.png',
+                      ),
                     ),
-                  ),
-                  const SizedBox(
-                    width: 20,
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Homer Simpson',
-                        style: GoogleFonts.poppins(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w600,
-                          color: const Color(0XFF555555),
+                    const SizedBox(
+                      width: 20,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Homer Simpson',
+                          style: GoogleFonts.poppins(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600,
+                            color: const Color(0XFF555555),
+                          ),
                         ),
-                      ),
-                      Text(
-                        'Administrador',
-                        style: GoogleFonts.poppins(
-                          fontSize: 14,
-                          fontWeight: FontWeight.w400,
-                          color: const Color(0XFF555555),
+                        Text(
+                          'Administrador',
+                          style: GoogleFonts.poppins(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                            color: const Color(0XFF555555),
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Divider(
-                color: Colors.black.withOpacity(0.75),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              ListTile(
-                leading: const Icon(
-                  CupertinoIcons.square_arrow_left,
-                  size: 34,
-                  color: Color(0XFF333333),
+                      ],
+                    ),
+                  ],
                 ),
-                title: Text(
-                  'Sair',
-                  style: GoogleFonts.poppins(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w600,
-                    color: const Color(0XFF555555),
-                  ),
+                const SizedBox(
+                  height: 10,
                 ),
-                onTap: () {},
-              ),
-            ],
+                Divider(
+                  color: Colors.black.withOpacity(0.75),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                _buildListTile(
+                  context: context,
+                  icon: CupertinoIcons.square_arrow_left,
+                  title: 'Sair',
+                  onTap: () {
+                    Navigator.of(context).popAndPushNamed('/auth/login');
+                  },
+                ),
+              ],
+            ),
           ),
           const SizedBox(
             height: 30,
           ),
         ],
       ),
+    );
+  }
+
+  Widget _buildListTile({
+    required BuildContext context,
+    required IconData icon,
+    required String title,
+    int? index,
+    required VoidCallback onTap,
+    bool enableNotifications = false,
+  }) {
+    return ValueListenableBuilder<int>(
+      valueListenable: selectedIndexNotifier,
+      builder: (context, selectedIndex, child) {
+        return ListTile(
+          leading: Icon(
+            icon,
+            size: 34,
+            color: const Color(0XFF555555),
+          ),
+          title: enableNotifications
+              ? Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      title,
+                      style: GoogleFonts.poppins(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                        color: const Color(0XFF555555),
+                      ),
+                    ),
+                    CircleAvatar(
+                      radius: 10,
+                      backgroundColor: Colors.red,
+                      child: Center(
+                        child: Text(
+                          '2',
+                          style: GoogleFonts.poppins(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              : Text(
+                  title,
+                  style: GoogleFonts.poppins(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                    color: const Color(0XFF555555),
+                  ),
+                ),
+          tileColor:
+              selectedIndex == index ? Colors.black.withOpacity(0.15) : null,
+          onTap: () {
+            selectedIndexNotifier.value = index!;
+            onTap;
+          },
+        );
+      },
     );
   }
 }
