822e86ee1b587d9228a9bbb39d363947f63624bd
Barba2k2 <lorenzo.dz@hotmail.com>
2024-06-26 11:36:24 -0300

    Code fixes

--- lib/app/pages/dashboard/widgets/card_widget.dart
+++ lib/app/pages/dashboard/widgets/card_widget.dart
@@ -7,69 +7,74 @@ class CardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: Colors.white,
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Lavagem completa',
-              style: GoogleFonts.inter(
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
-              ),
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: Colors.white,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Lavagem completa',
+            style: GoogleFonts.inter(
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
             ),
-            const SizedBox(
-              height: 8,
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    const Icon(
+                      Icons.person_rounded,
+                      color: Colors.black87,
+                    ),
+                    const SizedBox(
+                      width: 4,
+                    ),
+                    Text(
+                      'João Maria da Silva',
+                      style: GoogleFonts.inter(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    const Icon(
+                      Icons.car_repair_rounded,
+                      color: Colors.black87,
+                    ),
+                    const SizedBox(
+                      width: 4,
+                    ),
+                    Text(
+                      'Fiat Uno Mille',
+                      style: GoogleFonts.inter(
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      const Icon(
-                        Icons.person_rounded,
-                        color: Colors.black87,
-                      ),
-                      const SizedBox(
-                        width: 2,
-                      ),
-                      Text(
-                        'João Maria da Silva',
-                        style: GoogleFonts.inter(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      const Icon(
-                        Icons.car_repair_rounded,
-                        color: Colors.black87,
-                      ),
-                      const SizedBox(
-                        width: 4,
-                      ),
-                      Text(
-                        'Fiat Uno Mille',
-                        style: GoogleFonts.inter(
-                          fontSize: 16,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            Text(
+          ),
+          const SizedBox(
+            height: 14,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 8.0),
+            child: Text(
               'Cheiro forte vindo do porta-malas',
               maxLines: 2,
               overflow: TextOverflow.ellipsis,
@@ -77,40 +82,40 @@ class CardWidget extends StatelessWidget {
                 fontSize: 18,
               ),
             ),
-            const SizedBox(
-              height: 16,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    const Icon(
-                      CupertinoIcons.clock,
-                      color: Colors.black,
-                    ),
-                    const SizedBox(
-                      width: 4,
-                    ),
-                    Text(
-                      '15 de maio às 10h',
-                      style: GoogleFonts.inter(
-                        fontSize: 16,
-                      ),
-                    ),
-                  ],
-                ),
-                Text(
-                  'R\$ 120,00',
-                  style: GoogleFonts.inter(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
+          ),
+          const SizedBox(
+            height: 14,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  const Icon(
+                    CupertinoIcons.clock,
+                    color: Colors.black,
                   ),
+                  const SizedBox(
+                    width: 6,
+                  ),
+                  Text(
+                    '15 de maio às 10h',
+                    style: GoogleFonts.inter(
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              ),
+              Text(
+                'R\$ 120,00',
+                style: GoogleFonts.inter(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
                 ),
-              ],
-            ),
-          ],
-        ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
